4bd45956cfa599d2d949d89cb556b9b776fdf3bc
Egor Kuznetsov <egorkuz1408@gmail.com>
2025-01-15 19:24:24 +0400

    Created main screen with recycler view fixed bugs

--- app/src/main/java/com/kzerk/shoppinglistapp/presentation/MainActivity.kt
+++ app/src/main/java/com/kzerk/shoppinglistapp/presentation/MainActivity.kt
@@ -4,18 +4,35 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.kzerk.shoppinglistapp.R
 
 class MainActivity : AppCompatActivity() {
 	private lateinit var viewModel: MainViewModel
+	private lateinit var shopListAdapter: ShopListAdapter
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
 		setContentView(R.layout.activity_main)
+		setupRecyclerView()
 		viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 		viewModel.shopList.observe(this) {
-			//Log.d("MyLog", it.toString())
+			shopListAdapter.shopList = it
 		}
 	}
+
+	private fun setupRecyclerView() {
+		val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+		with(rvShopList) {
+			shopListAdapter = ShopListAdapter()
+			adapter = adapter
+			recycledViewPool
+				.setMaxRecycledViews(ShopListAdapter.VIEW_TYPE_ENABLED, ShopListAdapter.MAX_POOL_SIZE)
+			recycledViewPool
+				.setMaxRecycledViews(ShopListAdapter.VIEW_TYPE_DISABLED, ShopListAdapter.MAX_POOL_SIZE)
+		}
+
+	}
+
 }
