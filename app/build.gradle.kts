1595d09e8cef5f936e829f331cdcc16b108504f2
Egor Kuznetsov <egorkuz1408@gmail.com>
2025-01-15 16:27:47 +0400

    Created ViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
 	namespace = "com.kzerk.shoppinglistapp"
-	compileSdk = 34
+	compileSdk = 35
 
 	defaultConfig {
 		applicationId = "com.kzerk.shoppinglistapp"
--- app/src/main/java/com/kzerk/shoppinglistapp/presentation/MainActivity.kt
+++ app/src/main/java/com/kzerk/shoppinglistapp/presentation/MainActivity.kt
@@ -3,19 +3,20 @@ package com.kzerk.shoppinglistapp.presentation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.kzerk.shoppinglistapp.R
 
 class MainActivity : AppCompatActivity() {
+	private lateinit var viewModel: MainViewModel
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
 		setContentView(R.layout.activity_main)
-		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-			insets
+		viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+		viewModel.shopList.observe(this) {
+			//Log.d("MyLog", it.toString())
 		}
+		viewModel.getShopList()
 	}
 }
